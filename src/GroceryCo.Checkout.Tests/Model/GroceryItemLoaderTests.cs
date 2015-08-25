92e3b6603b8eeaf62614cd9a05dd4772dced7332
dave-prentice <dave@leadtrumpet.ca>
2015-08-24 21:08:36 -0600

    Add GroceryItem loader and tests

---
+++ src/GroceryCo.Checkout.Tests/Model/GroceryItemLoaderTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using GroceryCo.Checkout.Model;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace GroceryCo.Checkout.Tests.Model
+{
+    internal static class GroceryItemLoaderTests
+    { 
+        [Test]
+        public static void Load_ValidJson_Succeeds()
+        {
+            //Arrange + Act
+            var stockItems = GroceryItemLoader.Load(ValidItemsJson)
+                .ToArray();
+
+            //Assert
+            Assert.That(stockItems.Length, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public static void Load_InvalidJson_Fails()
+        {
+            //Arrange + Act + Assert
+            Assert.Throws<JsonSerializationException>(() => GroceryItemLoader.Load(InvalidItemsJson));
+        }
+
+
+        private const string ValidItemsJson = "[{id: 'Apple', price: 0.50}, {id: 'Orange', price: 1.0}]";
+
+        private const string InvalidItemsJson = "{foo: 'bar'}";
+    }
+}
