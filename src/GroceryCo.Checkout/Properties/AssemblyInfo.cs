197b4c9e243c6b0c40269234a1bb579426a5cd12
dave-prentice <dave@leadtrumpet.ca>
2015-08-24 19:17:52 -0600

    Initial check in

---
+++ src/GroceryCo.Checkout/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GroceryCo.Checkout")]
+[assembly: AssemblyProduct("GroceryCo.Checkout")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("GroceryCo.Checkout.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f56c98c48e6d6feccafe1ad991570e92bbe32c0bc239e4f0551e578f5ed216213cbb32850fbe8518d52962d908f28d5f5c5f3f327eb65c13e180e3189169f05fdbfad96cbca1d90f9a12d94b8a0c9fbd232b35ae5a86063bb480242eda019ce764e025a076a7885517bb8f73fa68ae3e87e9b9dc26c4d80b2eba351241721ab0")]
